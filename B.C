300cb14c28b08cb95c325f5d5641b8e9763a5586
arijit-pande <pande.arijit@gmail.com>
2014-06-09 09:22:56 -0400

    Birthday paradox

---
+++ B.C
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+#define NO_OF_TRIALS 100000
+//1 lakh trial runs . To improve accuracy increase to 10 lakhs or 1 crore
+int main()
+{
+	int hash[365],n,index;
+	int j;
+	unsigned long long int cnt=0,i=0;
+	printf("\nEnter number of people: ");
+	scanf("%d",&n);
+	srand(time(NULL));
+	for(i=0;i<NO_OF_TRIALS;i++)
+	{
+		for(j=0;j<365;j++)
+		hash[j]=-1;
+		for(j=0;j<n;j++)
+		{
+			index=rand()%365;
+			if(hash[index]==1)
+			{
+				cnt++;
+				break;
+			}
+			hash[index]=1;
+		}
+	}
+	printf("\n\nProbability(%%)= %lf%%\n\n",((cnt*1.0)/NO_OF_TRIALS)*100.0);
+
+	return 0;
+}
